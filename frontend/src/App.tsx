f8ac2e11d6ae10c5cd1803927a0b308aa0db414e
Sonjay Lake <lakesonjay@gmail.com>
2024-03-08 18:46:49 -0500

    Create registration form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { Navigate, Router, Routes, Route, BrowserRouter } from "react-router-dom"
 import Layout from "./layouts/Layout"
+import Register from "./pages/Register"
 
 function App() {
 
@@ -18,6 +19,11 @@ function App() {
                     </Layout>
           }/>
           <Route path="*" element={<Navigate to="/"/>}/>
+          <Route path="/register" element={
+          <Layout>
+            <Register/>
+          </Layout>
+         }></Route>
 
         </Routes>
         
