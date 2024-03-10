65552c06fbe0765be295699cc65f5c62f60acc59
Sonjay Lake <lakesonjay@gmail.com>
2024-03-09 21:30:10 -0500

    Initialize context provider, start using prettier

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,22 +2,19 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
-import userRouter from "./routes/users"
+import userRouter from "./routes/users";
 import authRouter from "./routes/auth";
 
 mongoose.connect(process.env.MONGO_CONNECTION as string);
 
-
 const app = express();
 app.use(express.json()); //convert body of requests to json automatically
-app.use(express.urlencoded({extended: true})); //helps for parsing url
+app.use(express.urlencoded({ extended: true })); //helps for parsing url
 app.use(cors()); //allows frontend and backend to communicate with each other
-
-
 
 app.use("/api/users", userRouter);
 app.use("/api/auth", authRouter);
 
 app.listen(8000, () => {
-    console.log(`server is running on port ${process.env.PORT}`)
-})
+  console.log(`server is running on port ${process.env.PORT}`);
+});
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,114 +1,133 @@
-import React from 'react';
-import {useForm} from "react-hook-form";
-import {useMutation} from "react-query";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { useMutation } from "react-query";
 import * as apiClient from "../api-clients";
 
-
 export type RegisterFormData = {
-    firstName: string,
-    lastName: string,
-    email: string,
-    password: string,
-    confirmPassword: string,
-}
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
 
 function Register() {
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterFormData>();
 
-    const {register, watch, handleSubmit, formState: {errors}} = useForm<RegisterFormData>();
+  const mutation = useMutation(apiClient.register, {
+    onSuccess: () => {
+      console.log("Registration successful!"); // for testing purposes
+    },
+    onError: (err: Error) => {
+      console.log("There was an issue registering: ", err);
+    },
+  });
 
-    const mutation = useMutation(apiClient.register,{
-        onSuccess: () => {
-            console.log("Registration successful!") // for testing purposes
-        },
-        onError: (err: Error) => {
-            console.log("There was an issue registering: ",err);
-        } 
-    });
+  const onSubmit = handleSubmit((data) => {
+    mutation.mutate(data);
+  });
 
-    const onSubmit = handleSubmit((data) => {
-        mutation.mutate(data);
-    });
-    
   return (
     <>
-        <form className="flex flex-col gap-5 " onSubmit={onSubmit}>
-            <h2 className="text-3xl font-bold">
-                Create an Account
-            </h2>
+      <form className="flex flex-col gap-5 " onSubmit={onSubmit}>
+        <h2 className="text-3xl font-bold">Create an Account</h2>
 
-            <div className="flex flex-col md:flex-row gap-5">
-                <label className="text-gray-700  text-sm text-bold flex-1">
-                    First Name
-                    <input className="border rounded w-full py-1 px-2 font-normal flex-1" {...register("firstName", {required: "This field is required"})}/>
-                    {errors.firstName && (
-                        <span className="text-red-500">{errors.firstName.message}</span>
-                    ) }
-                </label>
-                <label className="text-gray-700 text-sm text-bold flex-1">
-                    Last Name
-                    <input className="border rounded w-full py-1 px-2 font-normal flex-1" {...register("lastName", {required: "This field is required"})}/>
-                    {errors.lastName && (
-                        <span className="text-red-500">{errors.lastName.message}</span>
-                    ) }
-                </label>
-            </div>
-                <label className="text-gray-700  text-sm text-bold flex-1">
-                Email
-                <input type="email" className="border rounded w-full py-1 px-2 font-normal flex-1" {...register("email", {required: "This field is required"})}/>
-                {errors.email && (
-                        <span className="text-red-500">{errors.email.message}</span>
-                    ) }
-                </label>
-                <label className="text-gray-700  text-sm text-bold flex-1">
-                Password
-                <input type="password" className="border rounded w-full py-1 px-2 font-normal flex-1" {...register(
-                    "password", 
-                    {
-                        required: "This field is required", 
-                        minLength: {
-                            value: 6, message: "Password must be at least 6 characters."
-                        }
-                    })
-                } autoComplete='on'/>
-                {errors.password && (
-                        <span className="text-red-500">{errors.password.message}</span>
-                    ) }
-                </label>
-                <label className="text-gray-700  text-sm text-bold flex-1">
-                Confirm Password
-                <input type="password" autoComplete='on' className="border rounded w-full py-1 px-2 font-normal flex-1" {...register(
-                    "confirmPassword", 
-                    {
-                        validate:(value) => {
-                            if (!value){
-                                return "This field is required."
-                            } else if (watch("password") != value) {
-                                return "The passwords do not match."
-                            } else {
+        <div className="flex flex-col md:flex-row gap-5">
+          <label className="text-gray-700  text-sm text-bold flex-1">
+            First Name
+            <input
+              className="border rounded w-full py-1 px-2 font-normal flex-1"
+              {...register("firstName", { required: "This field is required" })}
+            />
+            {errors.firstName && (
+              <span className="text-red-500">{errors.firstName.message}</span>
+            )}
+          </label>
+          <label className="text-gray-700 text-sm text-bold flex-1">
+            Last Name
+            <input
+              className="border rounded w-full py-1 px-2 font-normal flex-1"
+              {...register("lastName", { required: "This field is required" })}
+            />
+            {errors.lastName && (
+              <span className="text-red-500">{errors.lastName.message}</span>
+            )}
+          </label>
+        </div>
+        <label className="text-gray-700  text-sm text-bold flex-1">
+          Email
+          <input
+            type="email"
+            className="border rounded w-full py-1 px-2 font-normal flex-1"
+            {...register("email", { required: "This field is required" })}
+          />
+          {errors.email && (
+            <span className="text-red-500">{errors.email.message}</span>
+          )}
+        </label>
+        <label className="text-gray-700  text-sm text-bold flex-1">
+          Password
+          <input
+            type="password"
+            className="border rounded w-full py-1 px-2 font-normal flex-1"
+            {...register("password", {
+              required: "This field is required",
+              minLength: {
+                value: 6,
+                message: "Password must be at least 6 characters.",
+              },
+            })}
+            autoComplete="on"
+          />
+          {errors.password && (
+            <span className="text-red-500">{errors.password.message}</span>
+          )}
+        </label>
+        <label className="text-gray-700  text-sm text-bold flex-1">
+          Confirm Password
+          <input
+            type="password"
+            autoComplete="on"
+            className="border rounded w-full py-1 px-2 font-normal flex-1"
+            {...register("confirmPassword", {
+              validate: (value) => {
+                if (!value) {
+                  return "This field is required.";
+                } else if (watch("password") != value) {
+                  return "The passwords do not match.";
+                } else {
+                }
+              },
+              required: "This field is required",
+              minLength: {
+                value: 6,
+                message: "Password must be at least 6 characters.",
+              },
+            })}
+          />
+          {errors.confirmPassword && (
+            <span className="text-red-500">
+              {errors.confirmPassword.message}
+            </span>
+          )}
+        </label>
 
-                            }
-
-
-                        },
-                        required: "This field is required", 
-                        minLength: {
-                            value: 6, message: "Password must be at least 6 characters."
-                        }
-                    })
-                }/>
-                {errors.confirmPassword && (
-                        <span className="text-red-500">{errors.confirmPassword.message}</span>
-                    ) }
-                </label>
-
-                <span>
-                    <button type="submit" className="bg-blue-600 text-white p-2 font-bold text-xl hover:bg-blue-500">
-                        Create Account
-                    </button>
-                </span>
-        </form>
+        <span>
+          <button
+            type="submit"
+            className="bg-blue-600 text-white p-2 font-bold text-xl hover:bg-blue-500"
+          >
+            Create Account
+          </button>
+        </span>
+      </form>
     </>
-  )
+  );
 }
 
-export default Register
+export default Register;
