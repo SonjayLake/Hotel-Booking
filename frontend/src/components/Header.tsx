7f9ea7e4ebbb2db70dc071d786be87a60f76a27f
Sonjay Lake <lakesonjay@gmail.com>
2024-03-09 23:04:31 -0500

    Cookie auth connection

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,21 +1,34 @@
-import React from 'react'
-import {Link} from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
+import { useAppContext } from "../contexts/AppContext";
 
 function Header() {
+  const { isLoggedIn } = useAppContext();
   return (
     <div className="bg-blue-800 py-6">
-        <div className="container mx-auto flex justify-between">
-            <span className="text-3xl text-white font-bold tracking-tight">
-                <Link to={"/"}>HotelBooking.com</Link>
-            </span>
-            <span className="flex space-x-2">
-                <Link to="/sign-in" className='flex items-center text-blue-600 px-3 font-bold hover:bg-grey-100 bg-white'>
-                    Sign-in
-                </Link>
-            </span>
-        </div>
+      <div className="container mx-auto flex justify-between">
+        <span className="text-3xl text-white font-bold tracking-tight">
+          <Link to={"/"}>HotelBooking.com</Link>
+        </span>
+        <span className="flex space-x-2">
+          {isLoggedIn ? (
+            <>
+              <Link to="/my-bookings">My Bookings</Link>
+              <Link to="/my-hotels">My Hotels</Link>
+              <button>Sign Out</button>
+            </>
+          ) : (
+            <Link
+              to="/sign-in"
+              className="flex items-center text-blue-600 px-3 font-bold hover:bg-grey-100 bg-white"
+            >
+              Sign-in
+            </Link>
+          )}
+        </span>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
