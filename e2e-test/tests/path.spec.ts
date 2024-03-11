690c241a28bd8eca304c9e58a4a27813bee3983b
Sonjay Lake <lakesonjay@gmail.com>
2024-03-11 08:08:35 -0400

    Add test-suite for signup/register

--- e2e-test/tests/path.spec.ts
+++ e2e-test/tests/path.spec.ts
@@ -1,18 +1,63 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+const URL = "http://localhost:5173/";
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test("should allow user sign-in", async ({ page }) => {
+  //navigate to homepage
+  await page.goto(URL);
+
+  //click on signin button
+  await page.getByRole("link", { name: "Sign-in" }).click();
+
+  //check to see if sign-in page has been navigated to
+  await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
+
+  await page.locator("[name=email]").fill("abc123@gmail.com"); //find an element on the page with a name of email and populate it
+  await page.locator("[name=password]").fill("abc123"); //find an element on the page with a name of password and populate it
+
+  await page.getByRole("button", { name: "Login" }).click();
+
+  await expect(page.getByText("Sign-in successful")).toBeVisible();
+
+  await expect(page.getByRole("link", { name: "My Bookings" })).toBeVisible();
+  await expect(page.getByRole("link", { name: "My Hotels" })).toBeVisible();
+  await expect(page.getByRole("button", { name: "Sign Out" })).toBeVisible();
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("should allow user to register", async ({ page }) => {
+  //use randomly generated email for robust tests
+  const testEmail = `test_register_${
+    Math.floor(Math.random() * 9000) + 1000
+  }@test.com`;
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  //navigate to homepage
+  await page.goto(URL);
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  //click sign-in button
+  await page.getByRole("link", { name: "Sign-in" }).click();
+
+  //click the registration link
+  await page.getByRole("link", { name: "Create an account here" }).click();
+
+  //check to see if on registration page
+  await expect(
+    page.getByRole("heading", { name: "Create an Account" })
+  ).toBeVisible();
+
+  //fill in the form
+  await page.locator("[name=firstName]").fill("e2e_first");
+  await page.locator("[name=lastName]").fill("e2e_first");
+  await page.locator("[name=email]").fill(testEmail);
+  await page.locator("[name=password]").fill("e2e123");
+  await page.locator("[name=confirmPassword]").fill("e2e123");
+
+  //click create account
+  await page.getByRole("button", { name: "Create Account" }).click();
+
+  //check to see if register successful
+  await expect(page.getByText("Registration Successful")).toBeVisible();
+
+  await expect(page.getByRole("link", { name: "My Bookings" })).toBeVisible();
+  await expect(page.getByRole("link", { name: "My Hotels" })).toBeVisible();
+  await expect(page.getByRole("button", { name: "Sign Out" })).toBeVisible();
 });
