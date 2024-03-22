603a09cc1a22038a6af97d432af3e805a3554848
Sonjay Lake <lakesonjay@gmail.com>
2024-03-21 23:14:15 -0400

    Add a 'view hotels' feature/page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,13 @@
-import {
-  Navigate,
-  Routes,
-  Route,
-  BrowserRouter,
-} from "react-router-dom";
+import { Navigate, Routes, Route, BrowserRouter } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Register from "./pages/Register";
 import SignIn from "./pages/SignIn";
 import AddHotel from "./pages/AddHotel";
 import { useAppContext } from "./contexts/AppContext";
+import MyHotels from "./pages/MyHotels";
 
 function App() {
-  const {isLoggedIn} = useAppContext();
+  const { isLoggedIn } = useAppContext();
   return (
     <>
       <BrowserRouter>
@@ -49,21 +45,28 @@ function App() {
             }
           />
 
-          {
-            isLoggedIn && (
+          {isLoggedIn && (
             <>
-              <Route 
-              path="/add-hotel" 
-              element={
-                <Layout>
-                  <AddHotel/>
-                </Layout>
-              }/>
-            </>)
-          }
+              <Route
+                path="/add-hotel"
+                element={
+                  <Layout>
+                    <AddHotel />
+                  </Layout>
+                }
+              />
+            </>
+          )}
+          <Route
+            path="/my-hotels"
+            element={
+              <Layout>
+                <MyHotels />
+              </Layout>
+            }
+          />
 
           <Route path="*" element={<Navigate to="/" />} />
-
         </Routes>
       </BrowserRouter>
     </>
