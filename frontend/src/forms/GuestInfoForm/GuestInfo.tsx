9939f6b053f9407b8765280a8003601d64ddf273
Sonjay Lake <lakesonjay@gmail.com>
2024-03-28 19:41:25 -0400

    WIP guest info form

---
+++ frontend/src/forms/GuestInfoForm/GuestInfo.tsx
@@ -0,0 +1,10 @@
+type Props = {
+  hotelId: string;
+  pricePerNight: number;
+};
+
+function GuestInfo({ hotelId, pricePerNight }): Props {
+  return <div>GuestInfo</div>;
+}
+
+export default GuestInfo;
